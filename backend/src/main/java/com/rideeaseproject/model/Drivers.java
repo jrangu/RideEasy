997bd9653c597fca0a382d882c2efbecf2b85e90
jrangu <55324369+jrangu@users.noreply.github.com>
2020-04-14 16:25:55 -0700

    Added primary key for riders table

--- backend/src/main/java/com/rideeaseproject/model/Drivers.java
+++ backend/src/main/java/com/rideeaseproject/model/Drivers.java
@@ -1,83 +1,87 @@
 package com.rideeaseproject.model;
 
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table
 
 public class Drivers {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 // TODO foreign key to get id from DriverLicense Table
 
-	private String startLocation;
-	private String endLocation;
-	private int capacity;
-	private int price;
-	private Date journeyDate;
-	
+    private String startLocation;
+    private String endLocation;
+    private int capacity;
+    private int price;
+    private Date journeyDate;
 
 
-public Drivers() {
-		
-	}
-	
-	public Drivers(String startLocation, String endLocation, int capacity,int price, Date journeyDate) {
-		super();
-		this.startLocation = startLocation;
-		this.endLocation = endLocation;
-		this.capacity = capacity;
-		this.price = price;
-		this.journeyDate = journeyDate;
-		
-	}
+    public Drivers() {
 
-	public String getstartLocation() {
-		return startLocation;
-	}
+    }
 
-	public void setstartLocation(String startLocation) {
-		this.startLocation = startLocation;
-	}
-	
-	public String getendLocation() {
-		return endLocation;
-	}
+    public Drivers(String startLocation, String endLocation, int capacity, int price, Date journeyDate) {
+        super();
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.capacity = capacity;
+        this.price = price;
+        this.journeyDate = journeyDate;
 
-	public void setendLocation(String endLocation) {
-		this.endLocation = endLocation;
-	}
+    }
 
-	public int getcapacity() {
-		return capacity;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setcapacity(int capacity) {
-		this.capacity = capacity;
-	}
-	
-	public int getprice() {
-		return price;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setprice(int price) {
-		this.price = price;
-	}
-	public Date getjourneyDate() {
-		return journeyDate;
-	}
+    public String getstartLocation() {
+        return startLocation;
+    }
 
-	public void setjourneyDate(Date journeyDate) {
-		this.journeyDate = journeyDate;
-	}
-	
+    public void setstartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getendLocation() {
+        return endLocation;
+    }
+
+    public void setendLocation(String endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public int getcapacity() {
+        return capacity;
+    }
+
+    public void setcapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getprice() {
+        return price;
+    }
+
+    public void setprice(int price) {
+        this.price = price;
+    }
+
+    public Date getjourneyDate() {
+        return journeyDate;
+    }
+
+    public void setjourneyDate(Date journeyDate) {
+        this.journeyDate = journeyDate;
+    }
+
 
 }
 
--- backend/src/main/java/com/rideeaseproject/model/Riders.java
+++ backend/src/main/java/com/rideeaseproject/model/Riders.java
@@ -1,94 +1,79 @@
 package com.rideeaseproject.model;
 
 
-
+import javax.persistence.*;
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table
 public class Riders {
-
-
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String firstname;
-	private String lastname;
-	private String pickup;
-	private String dropoff;
-	private Date journeyDate;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String pickup;
+    private String dropoff;
+    private Date journeyDate;
 
-	
-	public long getId() {
-		return id;
-	}
+    public Riders() {
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
-	
+    public Riders(String firstname, String lastname, String pickup, String dropoff, Date journeyDate) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.pickup = pickup;
+        this.dropoff = dropoff;
+        this.journeyDate = journeyDate;
+    }
 
+    public long getId() {
+        return id;
+    }
 
-	public Riders() {
-		
-	}
-	
-	public Riders(String firstname, String lastname, String pickup,String dropoff, Date journeyDate ) {
-		super();
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.pickup = pickup;
-		this.dropoff = dropoff;
-		this.journeyDate = journeyDate;
-		
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getFirstname() {
-		return firstname;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	
-	public String getLastname() {
-		return lastname;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public String getpickup() {
-		return pickup;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-	public void setpickup(String pickup) {
-		this.pickup = pickup;
-	}
-	
-	public String getdropOff() {
-		return dropoff;
-	}
+    public String getpickup() {
+        return pickup;
+    }
 
-	public void setdropOff(String dropoff) {
-		this.dropoff = dropoff;
-	}
-	public Date getjourneyDate() {
-		return journeyDate;
-	}
+    public void setpickup(String pickup) {
+        this.pickup = pickup;
+    }
 
-	public void setjourneyDate(Date journeyDate) {
-		this.journeyDate = journeyDate;
-	}
-	
+    public String getdropOff() {
+        return dropoff;
+    }
+
+    public void setdropOff(String dropoff) {
+        this.dropoff = dropoff;
+    }
+
+    public Date getjourneyDate() {
+        return journeyDate;
+    }
+
+    public void setjourneyDate(Date journeyDate) {
+        this.journeyDate = journeyDate;
+    }
 
 }
 
