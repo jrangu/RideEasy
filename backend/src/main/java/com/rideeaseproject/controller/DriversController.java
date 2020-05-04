45eb171e171cd0fa3c39526e684efbb743b3bfb9
alekhyaved <leelaalekhya@gmail.com>
2020-05-04 03:57:24 -0700

    Driver detail Confirmation to rider  for a booking backend

--- backend/src/main/java/com/rideeaseproject/controller/DriversController.java
+++ backend/src/main/java/com/rideeaseproject/controller/DriversController.java
@@ -5,6 +5,7 @@ import com.rideeaseproject.service.DriversService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,4 +20,15 @@ public class DriversController {
     public List<Drivers> getDriversList(){
         return driversService.getDriverPosts();
     }
+    
+
+	@GetMapping(path="/driverConfirmation")
+	public Drivers getDriverConfirmation(@RequestParam String email,  @RequestParam int trip_id ){
+	
+		System.out.println("driver controller with rider email and trip id" + email + trip_id);
+		Drivers driver = driversService.getDriverConfirmation(email, trip_id);
+		System.out.println("driver controller with driver detail" + driver);
+		return driver;
+	}
+    
 }
--- backend/src/main/java/com/rideeaseproject/repository/BookingsRepo.java
+++ backend/src/main/java/com/rideeaseproject/repository/BookingsRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rideeaseproject.model.Bookings;
+import com.rideeaseproject.model.Drivers;
 import com.rideeaseproject.model.Riders;
 import com.rideeaseproject.model.Trip;
 
@@ -26,4 +27,10 @@ public interface BookingsRepo extends JpaRepository<Bookings, Integer> {
     @Query("select b from Bookings b where b.driver.email =?1 and b.trip.id = ?2")
     
     public List<Bookings> getRidersForTrip(String email, int trip_id);
+    
+    @Query("select b from Bookings b where b.rider.email =?1 and b.trip.id =?2")
+  public Bookings getDriverConfirmation(String email, int trip_id);
+
+    
+    
 }
