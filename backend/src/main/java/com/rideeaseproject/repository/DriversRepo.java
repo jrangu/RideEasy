78ce6bd6e4e935aa358adfaac89b366d4fd26b27
anvithakp <anvitha135@gmail.com>
2020-05-08 00:31:00 -0700

    drivers repo

--- backend/src/main/java/com/rideeaseproject/repository/DriversRepo.java
+++ backend/src/main/java/com/rideeaseproject/repository/DriversRepo.java
@@ -1,10 +1,13 @@
 package com.rideeaseproject.repository;
 
 import com.rideeaseproject.model.Drivers;
+import com.rideeaseproject.model.License;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
 
 
 @Repository
@@ -12,5 +15,11 @@ public interface DriversRepo extends JpaRepository<Drivers, Integer> {
 
     @Query("SELECT d FROM Drivers d WHERE LOWER(d.email) = LOWER(:email)")
     public Drivers getDriverByEmail(@Param("email") String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update drivers set license_id= ?1 where email=?2",
+            nativeQuery = true)
+    void saveLicenseId(License license, String email);
 
 }
